07240f1f4771797b7c15b532db160a009cbdcf86
Noel Hack <Noel.hack12@gmail.com>
2024-02-02 10:38:46 +0100

    upload of audio files and fourier transformation

--- pages/Upload_music.py
+++ pages/Upload_music.py
@@ -18,5 +18,23 @@ st.title(page_title + " " + page_icon)
 
 
 
+# Datei hochladen
 audio = st.file_uploader("Upload an audio file", type=["mp3"])
 
+if audio:
+    # Audiodatei laden
+    y, sr = librosa.load(audio, sr=None)
+
+    # Fourier-Transformation durchführen
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+
+    # Zeit-Frequenz-Darstellung erstellen
+    fig, ax = plt.subplots(figsize=(10, 4))
+    img = librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log', ax=ax)
+    plt.colorbar(img, format='%+2.0f dB')  # Hier wird img als mappable übergeben
+    plt.title('Spectrogram')
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    st.pyplot(fig)
+
+    
