1ca61cccd8a96d230bf558915aeb774685ce9f31
Noel Hack <Noel.hack12@gmail.com>
2024-02-02 09:21:58 +0100

    Home von streamlit set up (sehr basic)

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# --- Home Page ---
+page_title = "shazam clone"
+page_icon = ":house:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
