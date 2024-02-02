7b8857458447353569ddea1aefd0390081aab310
Noel Hack <Noel.hack12@gmail.com>
2024-02-02 09:28:22 +0100

    streamlit page setup

--- Home.py
+++ Home.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
 # --- Home Page ---
-page_title = "shazam clone"
+page_title = "Shazam clone"
 page_icon = ":house:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "centered"
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
+
+
+col1, col2 = st.columns(2)
+if col1.button("Upload Music", use_container_width=True):
+    st.switch_page("Recognice_music.py")    
+if col2.button("Recognice Music", use_container_width=True):
+    st.switch_page("Upload_music.py") 
