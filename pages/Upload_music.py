bb56db497fced7db9d45588615070133a4ba5622
Sebastian Bachlechner <sebbachlechner47@gmail.com>
2024-02-15 23:51:53 +0100

    Import songs would now work with sql, but the ancor points and target points from the hashmap are not just numbers...

--- pages/Upload_music.py
+++ pages/Upload_music.py
@@ -7,7 +7,7 @@ from scipy.ndimage import maximum_filter
 import settings as set
 from audio_process import process_audio 
 from audio_process import create_hashes_v1
-#from songs import Song
+from song import Song
 
 import sqlite3
 
@@ -28,32 +28,14 @@ with st.form("entry_form", clear_on_submit=True):
     submitted = st.form_submit_button("Neuen Song speichern")
 
 if audio and submitted:
-    name_artist = song_name + "_" + song_artist
     fig, indices, times = process_audio(audio)
     hashmap = create_hashes_v1(indices, times) 
     st.pyplot(fig)
     #print(hashmap)
-    
-    converted_hashes = []
-    for hash_tuple in hashmap:
-        converted_tuple = tuple(int(value) for value in hash_tuple)
-        converted_hashes.append(converted_tuple)
-    
 
-    con = sqlite3.connect('database.db')
 
-    cursor = con.cursor()
+    song = Song(song_name, song_artist, hashmap)
+    song.save()
     
-    cursor.execute("""CREATE TABLE IF NOT EXISTS songs(
-    name text,
-    artist text
-    )""")
-    cursor.execute("INSERT INTO songs(name, artist) VALUES (?, ?)", (song_name, song_artist))
-    
-    cursor.execute("CREATE TABLE IF NOT EXISTS {} ('anchor point' INTEGER, 'target_point' INTEGER, 'delta time' INTEGER, time INTEGER)".format(name_artist))
-    for hash in converted_hashes:
-        cursor.execute("INSERT INTO {} ('anchor point', 'target_point', 'delta time', time) VALUES (?, ?, ?, ?)".format(name_artist), hash)
-    con.commit()
-    con.close()
 
 
