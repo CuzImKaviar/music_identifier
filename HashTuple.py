72dffd8e06556fa5a739dace74f7eca9921d8d53
Sebastian Bachlechner <sebbachlechner47@gmail.com>
2024-02-16 13:51:26 +0100

    deleteHashTuple-Class, because there is no need for the class anymore

--- HashTuple.py
+++
@@ -1,6 +0,0 @@
-class HashTuple:
-    def __init__(self, anchor_point, target_point, delta_time, time):
-        self.anchor_point = anchor_point
-        self.target_point = target_point
-        self.delta_time = delta_time
-        self.time = time
