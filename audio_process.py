6e52d2ced916e57c55c7b832d84a0a362a928764
Sebastian Bachlechner <sebbachlechner47@gmail.com>
2024-02-19 23:00:23 +0100

    list comprehension may be faster than for loops

--- audio_process.py
+++ audio_process.py
@@ -7,15 +7,11 @@ from scipy.ndimage import maximum_filter
 import settings as set
 
 def process_audio(audio_file):
-   
     # Load the audio file with librosa
-    y, sr = librosa.load(audio_file, sr=set.SAMPLE_RATE)  # Setzen der Samplingrate auf den festen Wert
+    y, sr = librosa.load(audio_file, sr=set.SAMPLE_RATE)
 
     # Fourier-Transformation durchführen
     D = librosa.amplitude_to_db(np.abs(librosa.stft(y, n_fft=set.FFT_WINDOW_SIZE)), ref=np.max)
-
-    # Anzahl der Frames im Spectrogramm
-    num_frames = D.shape[1]
 
     # Use maximum filter to highlight peaks in the spectrogram
     max_filtered = maximum_filter(D, size=(set.PEAK_BOX_SIZE, set.PEAK_BOX_SIZE + set.TEST), mode='constant', cval=-np.inf)
@@ -24,10 +20,10 @@ def process_audio(audio_file):
     peaks_indices = np.argwhere((D == max_filtered) & (D > set.MIN_DB_FILTER))
 
     # Berechne die Dauer eines Frames in Sekunden
-    frame_duration = librosa.get_duration(y=y, sr=sr) / num_frames
+    frame_duration = librosa.get_duration(y=y, sr=sr) / D.shape[1]
 
-    # Erzeuge eine Liste von Zeitstempeln für jedes Frame
-    times = np.arange(0, num_frames) * frame_duration
+    # Erzeuge eine Liste von Zeitstempeln für die gesamte Dauer
+    times = np.arange(0, D.shape[1]) * frame_duration
 
     # Zeit-Frequenz-Darstellung erstellen
     fig, ax = plt.subplots(3, 1, figsize=(10, 12))
@@ -52,27 +48,17 @@ def process_audio(audio_file):
     ax[2].set_xlabel('Time (die stimmt)')
     ax[2].set_ylabel('Frequency')
 
-    return fig, peaks_indices,times
-
-
-
+    return fig, peaks_indices, times
 
 def create_hashes_v1(peaks_indices, times):
     time_resolution = set.TARGET_T
     frequency_resolution = set.TARGET_F
     delay = set.TARGET_START_DELAY
 
-    hashes = []
-    for i, anchor_point in enumerate(peaks_indices):
-        anchor_time = times[anchor_point[1]]  # Zeitpunkt des Ankerpunkts
-        for j, target_point in enumerate(peaks_indices[i + 1:], start=i+1):
-            target_time = times[target_point[1]]  # Zeitpunkt des Zielpeaks
-            # Überprüfen, ob der Zeitpunkt des Zielpeaks später als der Zeitpunkt des Ankerpunkts ist
-            if target_time > anchor_time:
-                # Prüfen, ob der Ziel-Punkt innerhalb der Zeit- und Frequenzauflösung um den Ankerpunkt liegt
-                if (target_point[0] - anchor_point[0]) >= delay and abs(target_point[0] - anchor_point[0] - delay) <= time_resolution and abs(target_point[1] - anchor_point[1]) <= frequency_resolution:
-                    # Hash-Tupel erstellen: (freq_A, freq_B, zeit_delta, Zeitpunkt von Ankerpunkt)
-                    hash_tuple = (int(anchor_point[1]), int(target_point[1]), target_time - anchor_time, anchor_time)
-                    # Das Hash-Tupel der Liste der Hashes hinzufügen
-                    hashes.append(hash_tuple)
+    hashes = [(int(anchor_point[1]), int(target_point[1]), target_time - anchor_time, anchor_time)
+              for i, anchor_point in enumerate(peaks_indices)
+              for j, target_point in enumerate(peaks_indices[i + 1:], start=i+1)
+              if (anchor_time := times[anchor_point[1]]) < (target_time := times[target_point[1]]) and
+              (target_point[0] - anchor_point[0]) >= delay and abs(target_point[0] - anchor_point[0] - delay) <= time_resolution and abs(target_point[1] - anchor_point[1]) <= frequency_resolution]
+
     return hashes
