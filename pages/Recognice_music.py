bf9b1b894878441cfd064650651493f30f32ca8f
Noel Hack <Noel.hack12@gmail.com>
2024-02-21 13:05:24 +0100

    i added that a youtube link is provided of the recognicesed musik

--- pages/Recognice_music.py
+++ pages/Recognice_music.py
@@ -8,12 +8,28 @@ import settings as set
 from audio_process import process_audio
 from audio_process import create_hashes_v1
 from song import Song
+import requests
+from urllib.parse import quote_plus
+
+def search_youtube_video(song_name, artist_name):
+    query = f"{song_name} {artist_name} official music video"
+    url = f"https://www.youtube.com/results?search_query={quote_plus(query)}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        video_id = None
+        start_index = response.text.find('{"videoRenderer":{"videoId":"')
+        if start_index != -1:
+            end_index = response.text.find('"', start_index + 30)
+            video_id = response.text[start_index + 29:end_index]
+        if video_id:
+            return f"https://www.youtube.com/watch?v={video_id}"
+    return None
+
 
 # -------------- SETTINGS --------------
 page_title = "Recognice"
 page_icon = ":musical_note:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "centered"
-
 
 
 
@@ -31,4 +47,13 @@ if audio and submitted:
     st.pyplot(fig)
     detected_song = Song.identify(hashmap)
     st.write(detected_song)
+
+    song_name = detected_song.title
+    artist_name = detected_song.artist
+    video_link = search_youtube_video(song_name, artist_name)
+    if video_link:
+        st.write("YouTube Video Link:", video_link)
+    else:
+        st.write("Kein passendes Video gefunden.")
+       
 
--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ SAMPLE_RATE = 44100
 MIN_DB_FILTER = -45#-50
 
 # Größe des Peak-Box-Filters
-PEAK_BOX_SIZE = 30 #30
-TEST = 10 #7
+PEAK_BOX_SIZE = 31 #30
+TEST = 11 #7
 
 # Höhe der Zielzone in Hz
 TARGET_F = float(4000)  #4000
