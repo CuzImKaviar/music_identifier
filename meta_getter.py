bc8b0fdb69cedf1b04e154ed01de9e939b5fdccf
Tobias Stummer <st5390@mci4me.at>
2024-02-28 18:33:30 +0100

    Update meta_getter.py added YouTubeMusic link to meta_daten

--- meta_getter.py
+++ meta_getter.py
@@ -1,4 +1,9 @@
+from typing import Dict
 from ytmusicapi import YTMusic
+from duckduckgo_search import DDGS
+from collections import namedtuple
+from urllib.parse import urlencode, urlunparse
+
 # from song import Song
 
 class Song_Metadata():
@@ -11,19 +16,20 @@ class Song_Metadata():
         data = Song_Metadata.get_metadata(self.title, self.artist)
 
         self.album = data['album']
-        self.album_id = data['album_id']
-        self.song_id = data['song_id']
         self.duration = data['duration']
         self.year = data['year']
         self.viewCount = data['viewCount']
         self.cover = data['cover']
+
+        self.album_id = data['album_id']
+        self.song_id = data['song_id']
     
-    def print_infos(self):
-        attrs = vars(song)
+    def print_infos(self) -> None:
+        attrs = vars(self)
         print('\n'.join("%s: %s" % item for item in attrs.items()))
         
     @classmethod
-    def get_metadata(cls, title : str, artist : str):
+    def get_metadata(cls, title : str, artist : str = None) -> Dict[str, str]:
         '''
         Findes a song using a unofficial API for YouTube Music and returns relevant meta data for the song
 
@@ -52,6 +58,37 @@ class Song_Metadata():
             'viewCount' : songinfo['viewCount'],
             'cover': songinfo['thumbnail']['thumbnails'][-1]['url']
             }
+    
+
+    @staticmethod
+    def get_YouTubeMusic(song_id : str, album : str, artist : str = None) -> Dict[str, str]:
+
+        # namedtuple to match the internal signature of urlunparse
+        Components = namedtuple(
+            typename='Components', 
+            field_names=['scheme', 'netloc', 'path', 'params', 'query', 'fragment']
+        )
+
+        song_url =  urlunparse(
+            Components(
+                scheme='https',
+                netloc='music.youtube.com',
+                path='/watch',
+                params='',
+                query=urlencode({'v': song_id}),
+                fragment=''
+            )
+        )
+
+        return {
+            'song':  song_url,
+            'album':  'dummy'
+        }
+
+
+    @staticmethod
+    def get_Spotify(title : str, album : str, artist : str = None) -> Dict[str, str]:
+        return
 
     def __str__(self):
         return f"{self.title} by {self.artist}"
@@ -63,6 +100,27 @@ if __name__ == "__main__":
     
     title = "Ghost Division"
     artist = "Sabaton"
+    album = "The Art of War (Re-Armed)"
 
     song = Song_Metadata(title, artist)
     song.print_infos()
+
+    print('\n')
+    print(f"URL: {Song_Metadata.get_YouTubeMusic(song.song_id, song.album)}")
+
+    # keywords = f"{album} {artist} YouTubeMusic" if artist else title
+    
+    # results = DDGS().text(
+    #     keywords,
+    #     region=None,
+    #     safesearch='off',
+    #     timelimit=None,
+    #     backend="api",
+    #     max_results=10)
+
+    # for result in results:
+    #     print('\n')
+    #     for x in result:
+    #         print (x,':',result[x])
+
+    
