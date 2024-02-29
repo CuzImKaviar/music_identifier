68b2b25384cb93bff69b7f458a74a3cd65a3b9a5
Sebastian Bachlechner <sebbachlechner47@gmail.com>
2024-02-29 20:21:34 +0100

    recognice_music in english

--- pages/Recognice_music.py
+++ pages/Recognice_music.py
@@ -11,28 +11,23 @@ from audio_process import plot_all
 from audio_recorder_streamlit import audio_recorder
 from song import Song
 
-
-
 # -------------- SETTINGS --------------
 page_title = "Recognice"
 page_icon = ":musical_note:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "centered"
 
-
-
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
-    
 
 option = st.radio(
-    'Wählen Sie eine Option:',
+    'Choose an option:',
     ('Upload file for music recognition', 'Microphone-based music recognition'))
 
 if option == 'Upload file for music recognition':
 
     with st.form("file-based", clear_on_submit=True):
         audio = st.file_uploader("Upload an audio clip", type=["mp3", "wav"])
-        submitted = st.form_submit_button("Song erkennen")
+        submitted = st.form_submit_button("Recognize song")
 
     if audio and submitted:
         try:
@@ -47,11 +42,11 @@ if option == 'Upload file for music recognition':
                     st.write("YouTube Video Link:", video_link)
                 else:
                     st.write(detected_song)
-                    st.write("Kein passendes Video gefunden.")
+                    st.write("No matching video found.")
             else:
-                st.error("Song konnte nicht erkannt werden.")
+                st.error("Song could not be recognized.")
         except Exception as e:
-            st.error(f"Fehler beim Erkennen des Songs: {e}")
+            st.error(f"Error recognizing the song: {e}")
 
 elif option == 'Microphone-based music recognition':
 
@@ -59,7 +54,7 @@ elif option == 'Microphone-based music recognition':
         audio_bytes = audio_recorder(energy_threshold=(-1.0, 1.0), pause_threshold=6.0, sample_rate=41_000)
         if audio_bytes:   
             audio = st.audio(audio_bytes, format="audio/wav")
-        submitted = st.form_submit_button("Song erkennen")
+        submitted = st.form_submit_button("Recognize song")
         
     if submitted:
         try:
@@ -76,8 +71,8 @@ elif option == 'Microphone-based music recognition':
                             st.write("YouTube Video Link:", video_link)
                         except Exception as e:
                             st.write(detected_song)
-                            st.write("Kein passendes Video gefunden.") 
+                            st.write("No matching video found.") 
                 except Exception as e:
-                    st.error("Song konnte nicht erkannt werden.")
+                    st.error("Song could not be recognized.")
         except Exception as e:
-            st.error(f"Fehler beim Erkennen des Songs: {e}")
+            st.error(f"Error recognizing the song: {e}")
