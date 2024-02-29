12b2cd05587c2e6df50cca0ccc919d5a5f5bed79
Sebastian Bachlechner <sebbachlechner47@gmail.com>
2024-02-29 20:19:54 +0100

    edit_music in english

--- pages/Edit_music.py
+++ pages/Edit_music.py
@@ -2,7 +2,6 @@ import streamlit as st
 from song import Song
 import sqlite3
 
-
 # -------------- SETTINGS --------------
 page_title = "Upload music"
 page_icon = ":microphone:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
@@ -14,16 +13,14 @@ st.title(page_title + " " + page_icon)
 all_songs = Song.get_all_songs()    
 
 with st.form("delete_form", clear_on_submit=True):
-    song_to_delete = st.selectbox('Song auswählen', options=all_songs, key="title")
+    song_to_delete = st.selectbox('Select song', options=all_songs, key="title")
 
-    submitted = st.form_submit_button("Titel löschen")
+    submitted = st.form_submit_button("Delete title")
     if submitted:
         try:
             title, artist = song_to_delete.split(" by ")
             song = Song(title, artist, [])
             song.delete()
-            st.write(f"{title} wurde erfolgreich gelöscht!")
+            st.write(f"{title} was successfully deleted!")
         except Exception as e:
-            st.write(f"Fehler beim Löschen von {song_to_delete}: {e}")
-
-        
+            st.write(f"Error deleting {song_to_delete}: {e}")
