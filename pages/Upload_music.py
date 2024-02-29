959de2fea4557184a24368692a9acfa6c8f5e7df
Sebastian Bachlechner <sebbachlechner47@gmail.com>
2024-02-29 20:14:11 +0100

    error handling upload_music and outsourcing wav_to_mp3() function

--- pages/Upload_music.py
+++ pages/Upload_music.py
@@ -10,8 +10,6 @@ from audio_process import plot_all
 from song import Song
 import time
 import sqlite3
-from pydub import AudioSegment
-from io import BytesIO
 
 
 
@@ -24,44 +22,41 @@ st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
 
 
-def mp3_to_wav(mp3_bytes):
-    audio = AudioSegment.from_mp3(BytesIO(mp3_bytes))
-    wav_bytes = audio.export(format="wav").read()
-    return wav_bytes
-
-
 with st.form("entry_form", clear_on_submit=True):
     # Datei hochladen
     song_name = st.text_input("Name des Songs", max_chars=64, placeholder="Name hier einfügen ...", key="Name")
     song_artist = st.text_input("Name des Künstlers", max_chars=64, placeholder="Künstler hier einfügen ...", key="Artist")
-    audio = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
-    
-    if audio is not None:
-        if audio.type == "audio/mp3":
-            mp3_data = audio.getvalue()
-            wav_data = mp3_to_wav(mp3_data)
-            audio = BytesIO(wav_data)
+    audio = st.file_uploader("Upload an audio file", type=["mp3", "wav"])           
         
     submitted = st.form_submit_button("Neuen Song speichern")
 
 
 if audio and submitted:
+    try:
+        if audio.type == "audio/mp3":
+                audio = Song.mp3_to_wav(audio)
+    except Exception as e:
+        st.write("Fehler beim Konvertieren der Audiodatei: {e}")
+        print("Fehler beim Konvertieren der Audiodatei: {e}")
 
     # Startzeitpunkt erfassen
     start_time = time.time()
 
+    try:
+        hashes = fingerprint_file(audio)
+        #fig = plot_all(audio, hashes)
+        #st.pyplot(fig)
 
-    hashes = fingerprint_file(audio)
-    #fig = plot_all(audio, hashes)
-    #st.pyplot(fig)
+        # Endzeitpunkt erfassen
+        end_time = time.time()
 
-    # Endzeitpunkt erfassen
-    end_time = time.time()
-    # Ausführungszeit berechnen
-    execution_time = end_time - start_time
-    st.write("Ausführungszeit:", execution_time, "Sekunden")
-    print("Ausführungszeit:", execution_time, "Sekunden")
-
+        # Ausführungszeit berechnen
+        execution_time = end_time - start_time
+        st.write("Ausführungszeit:", execution_time, "Sekunden")
+        print("Ausführungszeit:", execution_time, "Sekunden")
+    except Exception as e:
+        st.write("Fehler beim Erstellen des Fingerabdrucks: {e}")
+        print("Fehler beim Erstellen des Fingerabdrucks: {e}")
 
     song = Song(song_name, song_artist, hashes)
     song.save()
